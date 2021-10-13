16f04e00fd649753d41b6836251fe8ffbd673753
Johan-Luca Rossi <johan@MacBook-Pro-de-Johan-Luca.local>
2021-10-13 19:52:35 +0200

    Branchement borneInf Ok

--- VertexCover.py
+++ VertexCover.py
@@ -41,22 +41,39 @@ def timeComplex(fonction,G):
 def displayComplex(fonction,nmax,title):
     plot1=plt.figure(title)
 
-    x=np.arange(nmax/10,nmax,nmax/10)
+    x=np.arange(1,nmax)
     for p in np.arange (0.1,0.9,0.1):
         y=[]
-        for i in range (nmax/10,nmax,nmax/10):
+        for i in range (1,nmax):
             sum=0
             for j in range (1,10):
                 sum+=timeComplex(fonction,generateGraphe(i,p))
             y.append(sum/10)
         plt.plot(x,y,label = p)
 
+    plt.xlabel('nb sommets')
+    plt.ylabel('temps(s)')
+    #plt.legend()
+    return plot1   
+
+def displayComplexFixedP(fonction,nmax,p,title):
+    plot1=plt.figure(title)
+
+    x=np.arange(1,nmax)
+    
+    y=[]
+    for i in range (1,nmax):
+        sum=0
+        for j in range (1,60):
+            sum+=timeComplex(fonction,generateGraphe(i,p))
+        y.append(sum/60)
+    plt.plot(x,y,label = p)
 
     plt.xlabel('nb sommets')
     plt.ylabel('temps(s)')
     #plt.legend()
     return plot1   
-    
+       
 
 
 def generateGraphe(n,p):
@@ -88,9 +105,17 @@ def couplageMax(G):
                 C.append(x)
                 C.append(y)
     return C
-            
-def branchement_aux(G, C):
+
+
+###########BRANCHEMENT#############
+bestCouverture = []
+noeudGen = 0
+
+def branchement_aux_1(G, C):
+    #print(G)
+
     global bestCouverture
+    global noeudGen
     sommetsSup = []
     for g in G:
         if len(G[g]) == 0:
@@ -107,29 +132,110 @@ def branchement_aux(G, C):
         C1 = copy.deepcopy(C)
         C2 = copy.deepcopy(C)
         C1.append(u)
-        branchement_aux(supprimerSommet(G1,u), C1)
+        noeudGen+=2
+        branchement_aux_1(supprimerSommet(G1,u), C1)
         C2.append(v)
-        branchement_aux(supprimerSommet(G2,v), C2)
+        branchement_aux_1(supprimerSommet(G2,v), C2)
 
-bestCouverture = []
 
 def branchement_1(G):
     global bestCouverture
+    global  noeudGen
+    noeudGen=0
     bestCouverture = [g for g in G]
     sommetsSup = []
     for g in G:
         if len(G[g]) == 0:
             sommetsSup.append(g)
     supprimerEnsembleSommet(G, sommetsSup)
+    if(len(G.keys()))==0:
+        return G
     u = list(G.keys())[0]
     v = G[u][0]
     G1 = copy.deepcopy(G)
     G2 = copy.deepcopy(G)
-    branchement_aux(supprimerSommet(G1,u), [u])
-    branchement_aux(supprimerSommet(G2,v), [v])
+    noeudGen+=2
+    branchement_aux_1(supprimerSommet(G1,u), [u])
+    branchement_aux_1(supprimerSommet(G2,v), [v])
     return bestCouverture
 
+########BONRE INF###########
+
+def calculBorneInf(G):
+    n=len(G)
+    m=0
+    for g in G: 
+        m+=len(G[g])
+    m=m/2
+    d=len(G[degMax(G)])
+    if(d!=0): #Graphe vide 
+        b1=np.ceil(m/d)
+    else:
+        b1=0
+    b2=len(couplageMax(G))/2
+    b3=(2*n-1-np.sqrt((np.square(2*n-1)-8*m)))/2
+
+    return (b1,b2,b3)
+
+def testBorneInf(C,G):
+    (b1,b2,b3)=calculBorneInf(G)
+    return len(C)+np.max([b1,b2,b3])<=len(bestCouverture)
         
+
+def branchement_aux_Couplage(G, C):
+    global bestCouverture
+    global noeudGen
+    sommetsSup = []
+    for g in G:
+        if len(G[g]) == 0:
+            sommetsSup.append(g)
+    supprimerEnsembleSommet(G, sommetsSup)
+    if len(G) == 0:
+        if len(bestCouverture) > len(C):
+            bestCouverture = C
+    else:
+        G1 = copy.deepcopy(G)
+        G2 = copy.deepcopy(G)
+        u = list(G.keys())[0]
+        v = G[u][0]
+        C1 = copy.deepcopy(C)
+        C2 = copy.deepcopy(C)
+
+        G1 = supprimerSommet(G1,u)
+        G2 = supprimerSommet(G2,v)
+
+        C1.append(u)
+        if(testBorneInf(C1,G1)):
+            noeudGen+=1
+            branchement_aux_Couplage(G1, C1)
+
+        C2.append(v)
+        if(testBorneInf(C2,G2)):
+            noeudGen+=1  
+            branchement_aux_Couplage(G2, C2)
+
+
+def branchement_Couplage(G): #Faire un test de borne inf a l'init si on init a glouton ...
+    global bestCouverture
+    global  noeudGen
+    noeudGen=0
+    bestCouverture = [g for g in G]
+    sommetsSup = []
+    for g in G:
+        if len(G[g]) == 0:
+            sommetsSup.append(g)
+    supprimerEnsembleSommet(G, sommetsSup)
+    if(len(G.keys()))==0:
+        return G
+    u = list(G.keys())[0]
+    v = G[u][0]
+    G1 = copy.deepcopy(G)
+    G2 = copy.deepcopy(G)
+    noeudGen+=2
+    branchement_aux_Couplage(supprimerSommet(G1,u), [u])
+    branchement_aux_Couplage(supprimerSommet(G2,v), [v])
+
+    return bestCouverture
 
 
 
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import VertexCover as vc
 import matplotlib.pyplot as plt
+import numpy as np
+import copy
 
-# G = {0 : [1,2], 1 : [0], 2 : [0]}
-G = vc.generateGraphe(10,0.3)
-C = vc.branchement_1(G)
-print(C)
+# = {0 : [1,2], 1 : [0,4], 2 : [0],3:[4],4:[1,3]}
+#G = vc.generateGraphe(20,0.3)
+
 #C = vc.couplageMax(G)
 #print(G)
 #print(C)
@@ -18,3 +19,24 @@ print(C)
 # vc.displayComplex(vc.algo_glouton,100,"glouton")
 # vc.displayComplex(vc.couplageMax,100,"couplage")
 # plt.show()
+
+#print(vc.algo_glouton(G))
+#C = vc.branchement_1(vc.generateGraphe(10,0.3))
+#print(C)
+#print("Noeud gen: ",vc.noeudGen)
+
+#vc.displayComplex(vc.branchement_1,15,"Branchement dumb")
+#plt.show()
+
+G=vc.generateGraphe(15,0.3)
+Gbis=copy.deepcopy(G)
+
+#G={0: [3, 8], 1: [3, 8], 2: [6], 3: [0, 1], 4: [5, 8], 5: [4, 8], 6: [2], 7: [], 8: [0, 1, 4, 5]}
+#print(G)
+print(1)
+print("SOL",vc.branchement_1(G))
+print(2)
+print("SOL",vc.branchement_Couplage(G))
+vc.displayComplex(vc.branchement_1,15,"Branchement couplage")
+plt.show()
+
